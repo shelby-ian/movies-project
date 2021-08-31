83d8af9aadd1e979e8d974d42af3679f7eeb805a
Ian Arellano <ian.g.arellano01@gmail.com>
2021-08-31 11:50:31 -0500

    add more to movies-project.js

--- js/movies-project.js
+++ js/movies-project.js
@@ -1,3 +1,24 @@
 "use strict";
 
-let url = 'https://mini-yielding-softball.glitch.me/movies';
+const url = 'https://mini-yielding-softball.glitch.me/movies';
+
+const displayMovie = (movie) => {
+	let html = '<div>'
+	html += `<p>${movie.rating}</p>`
+	html += `<p>${movie.title} (${movie.year})</p>`
+	html += `<p>${movie.genre}</p>`
+	html += `<p>${movie.plot}</p>`
+	html += '</div>'
+
+	return html;
+}
+
+const getMovies = () => fetch(url)
+	.then(response => response.json())
+	.catch(console.error);
+
+
+getMovies().then(movies => movies.forEach(movie => {
+	$('body').append(displayMovie(movie));
+	// console.log(movie);
+}))
