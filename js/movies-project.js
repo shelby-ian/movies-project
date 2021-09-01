a2f4c6ba59fb61ad61b1edc9ada78d1927ba5b82
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-08-31 19:46:10 -0500

    start making movies editable

--- js/movies-project.js
+++ js/movies-project.js
@@ -1,66 +1,105 @@
 "use strict";
 $(document).ready(() => {
-	const results = $("#results");
+  const results = $("#results");
 
-const url = 'https://mini-yielding-softball.glitch.me/movies';
+  const url = "https://mini-yielding-softball.glitch.me/movies";
 
-const displayMovie = (movie) => {
-	let html = '<div>'
-	html += `<p>${movie.rating}</p>`
-	html += `<p>${movie.title} (${movie.year})</p>`
-	html += `<p>${movie.genre}</p>`
-	html += `<p>${movie.plot}</p>`
-	html += '</div>'
+  const ratingInput = (rating, value) => `
+<div class="input">
+		  	label>Rating</label>
+			<input type="checkbox" class="rating" value=${rating} checked="${
+    parseInt(rating) === 1
+  }" />
+			<input type="checkbox" class="rating" value=${rating} />
+			<input type="checkbox" class="rating" value=${rating} />
+			<input type="checkbox" class="rating" value=${rating} />
+			<input type="checkbox" class="rating" value=${rating} />
+		  <div class="input">
+`;
 
-	return html;
-}
+  const displayMovie = ({
+    rating = 1,
+    title = "New movie",
+    year,
+    genre,
+    plot,
+  }) => {
+    const html = `
+	  	<div class="movie">
+		  <div class="input">
+			<label>Title</label>
+			<input class="title" value=${title} />
+		  </div>
 
-const getMovies = () => fetch(url)
-	.then(response => response.json())
-	.catch(console.error);
+		</div>
+	  `;
+    // let html = '<div class="movie">';
+    // if (rating) html += `<p>${rating}</p>`;
+    // if (title) html += `<p>${title} (${year ?? "None"})</p>`;
+    // if (genre) html += `<p>${genre}</p>`;
+    // if (plot) html += `<p>${plot}</p>`;
+    // html += "</div>";
 
-let showMovies = (movies) => {
-	results.html('')
-	results.append(
-		movies.map((movie) => {
-			return displayMovie(movie)
-		})
-	)
-}
+    return html;
+  };
 
-getMovies().then(movies => showMovies(movies));
-//"title": "down",
-//       "rating": "5",
-//       "poster": "https://m.media-amazon.com/images/M/MV5BYWMwMzQxZjQtODM1YS00YmFiLTk1YjQtNzNiYWY1MDE4NTdiXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg",
-//       "year": "2001",
-//       "genre": "Drama, History, War",
-//       "director": "Ridley Scott",
-//       "plot": "160 elite U.S. soldiers drop into Somalia to capture two top lieutenants of a renegade warlord and find themselves in a desperate battle with a large force of heavily-armed Somalis.",
-//       "actors": "Josh Hartnett, Ewan McGregor, Tom Sizemore, Eric Bana",
-//       "id": 2
+  const showMovies = (movies) => {
+    results.html("");
+    results.append(
+      movies.map((movie) => {
+        return displayMovie(movie);
+      })
+    );
+  };
 
+  const getMovies = () =>
+    fetch(url)
+      .then((response) => response.json())
+      .catch(console.error);
 
-const addMovie = (movie) => fetch(url, {
-	method: 'POST',
-	headers: {
-		'Content-Type': 'application/json'
-	},
-	body: JSON.stringify(movie)
-})
-	.then(res => res.json())
-	.then(data => {
-		console.log(`Success: created ${JSON.stringify(data)}`);
-		return data.id; // to access the primary key of the newly created entity
-	})
-	.catch(console.error);
-const testMovie = {
-		title: 'superman',
-		rating: 10
-	}
+  const update = (id, data) =>
+    fetch(`${url}/${id}`, {
+      method: "PATCH",
+      data,
+    });
 
-$('#add-movie').click(addMovie(testMovie));
+  const remove = (id) =>
+    fetch(`${url}/${id}`, {
+      method: "DELETE",
+    }).then(showMovies);
 
+  getMovies().then((movies) => {
+    console.log(movies);
+    showMovies(movies);
+  });
+  //"title": "down",
+  //       "rating": "5",
+  //       "poster": "https://m.media-amazon.com/images/M/MV5BYWMwMzQxZjQtODM1YS00YmFiLTk1YjQtNzNiYWY1MDE4NTdiXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg",
+  //       "year": "2001",
+  //       "genre": "Drama, History, War",
+  //       "director": "Ridley Scott",
+  //       "plot": "160 elite U.S. soldiers drop into Somalia to capture two top lieutenants of a renegade warlord and find themselves in a desperate battle with a large force of heavily-armed Somalis.",
+  //       "actors": "Josh Hartnett, Ewan McGregor, Tom Sizemore, Eric Bana",
+  //       "id": 2
 
+  const addMovie = (movie) =>
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(movie),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(`Success: created ${JSON.stringify(data)}`);
+        return data.id; // to access the primary key of the newly created entity
+      })
+      .catch(console.error);
+  const testMovie = {
+    title: "superman",
+    rating: 10,
+  };
 
-
+  $("#add-movie").click(() => addMovie(testMovie));
 });
